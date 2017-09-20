9a4c7586f7afc5984b9853d2e36e06630ef4d05d
20'cents <vpaveau@outlook.com>
2017-09-20 21:29:19 +0200

    in progress

--- src/Iot/Controllers/Controller.php
+++ src/Iot/Controllers/Controller.php
@@ -4,18 +4,36 @@ namespace Croak\Iot\Controllers;
 use \Psr\Http\Message\ResponseInterface as Response;
 use \Slim\Container;
 
+/**
+* base Controller for all controlers*/
 class Controller
 {
+    /**
+    *@var \Slim\Container 
+    */
     private $container;
 
+    /**
+    * instantiates the slim container
+    * @param \Slim\Container $container the slim container
+    */
     public function __construct(Container $container){
         $this->container = $container;
     }
 
+    /**
+    * logs messages in the logger with a debug level
+    * @param String $message message to log
+    */
     public function debug($message){
         return $this->container->logger->debug($message);
     }
 
+    /**
+    * returns a server error due to an issue with the server or the app
+    * @param \Psr\Http\Message\ResponseInterface $response the response interface
+    * @param String $message a message to add in the body
+    */
     public function serverError(Response $response, $message){
         $this->debug($message);
         $body = $response->getBody();
@@ -23,6 +41,9 @@ class Controller
         return $response->withStatus(500);
     }
 
+    /**
+    * re
+    */
     public function requestError(Response $response, $message){
         $this->debug($message);
         $body = $response->getBody();
